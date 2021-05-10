8e605c5b6de6c3cce5719982d2d0ea1cc2b04048
OMEN2 <1617218599@qq.com>
2021-05-10 20:27:13 +0800

    add radius change color

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,22 +21,40 @@ var ms = [
   ],
 ];
 
+var ms2 = [
+  [// 0
+    '第二教学楼', '第三教学楼'
+  ],
+  [// 1
+      [// 1 0
+        '一层', '二层'
+      ],
+      [// 1 1
+        '三层','四层'
+      ],
+  ],
+];
+
 Page({
   data: {
-    currentTab:0,
     multiArray: [
       ['电子设备', '证件','个人物品','学习用品','全部物品'],
       ['手机', '平板电脑', 'kindle', '笔记本电脑', '耳机','充电器','充电宝','数据线','手表','u盘','鼠标','键盘','触控笔','其他']
   ],
+    multiArray2: [
+      ['第二教学楼', '第三教学楼'],
+      ['一层', '二层']
+  ],
   multiIndex: [0, 0, 0],
+  multiIndex2: [0, 0, 0],
   },
   onLoad: function () {
     console.log('Welcome to Mini Code')
   },
   postLostConfirm: function(){
     tt.showModal({
-      title: '您希望请求一个失物查询请求',
-      content: '目前您的失物还未被拾取，若稍后出现符合您填写特征的物品被拾到，我们将以消息的形式通知您',
+      title: '您希望发送一个失物查询请求',
+      content: '如果您在下方列表中未找到丢失物品，可能是您的失物还未被拾取。若稍后出现符合您填写特征的物品被拾到，我们将以消息的形式通知您',
       success (res) {
           if (res.confirm) {
               console.log('confirm, continued');
@@ -82,6 +100,12 @@ Page({
         multiIndex: e.detail.value
     })
   },
+  bindMultiPickerChange2: function (e) {
+    console.log('picker2发送选择改变，携带值为', e.detail.value)
+    this.setData({
+        multiIndex2: e.detail.value
+    })
+  },
   bindMultiPickerColumnChange: function (e) {
       // return;
       console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
@@ -108,6 +132,32 @@ Page({
       }
       this.setData(data);
     },
+  bindMultiPickerColumnChange2: function (e) {
+      // return;
+      console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+      var data = {
+          multiArray2: this.data.multiArray2,
+          multiIndex2: this.data.multiIndex2
+      };
+      switch (e.detail.column) {
+          case 0:
+              data.multiIndex2[0] = e.detail.value;
+              data.multiIndex2[1] = 0;
+              data.multiIndex2[2] = 0;
+              data.multiArray2[1] = ms2[1][data.multiIndex2[0]];
+              // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
+              break;
+          case 1:
+              data.multiIndex2[1] = e.detail.value;
+              data.multiIndex2[2] = 0;
+              // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
+              break;
+          case 2:
+              // data.multiIndex[2] = e.detail.value;
+              break;
+      }
+      this.setData(data);
+    },
 
     onLoad: function (options) {
      
@@ -115,6 +165,19 @@ Page({
     tt.login({
       success (res) {
           console.log(`login 调用成功 ${res.code} `);
+          tt.request({
+            url: 'https://www.fengzigeng.com/api/minilogin', // 目标服务器url
+            method: "POST",
+            header:{
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+              'code':res.code
+            },
+            success: (res) => {
+              console.log(res);
+            }
+          });
       },
       fail (res) {
           console.log(`login 调用失败`);
