7c41165954f32cdb98280f594e57c0b44ba16b41
OMEN2 <1617218599@qq.com>
2021-05-12 17:25:35 +0800

    finish lost page

--- pages/lost/index.js
+++ pages/lost/index.js
@@ -23,39 +23,76 @@ var ms1 = [
 
 var ms2 = [
     [// 0
-      '电子设备', '证件','个人物品','学习用品','全部物品'
+      '宿舍区', '教学区'
     ],
     [// 1
         [// 1 0
-          '手机', '平板电脑', 'kindle', '笔记本电脑', '耳机','充电器/宝/数据线','手表','u盘','鼠标','键盘','触控笔','全部电子设备'
+          '14栋', '16栋'
         ],
         [// 1 1
-          '一卡通','身份证','学生证','健身卡','全部证件'
+          '二教','三教'
+        ]
+    ],
+  ];
+var ms3 = [
+    [// 0
+      '可选','宿舍区', '教学区'
+    ],
+    [// 1
+        [],
+        [// 1 0
+          '14', '16'
         ],
-        [
-          '衣物','包','眼镜','钱包','钥匙','水杯','雨伞','化妆品','首饰','全部个人物品'
+        [// 1 1
+          '二','四'
+        ]
+    ],
+  ];
+var ms4 = [
+    [// 0
+      '可选','宿舍区', '教学区'
+    ],
+    [// 1
+        [],
+        [// 1 0
+          '14', '16'
         ],
-        [
-          '笔袋','书籍','笔记本','全部学习用品'
-        ],
-        [
-          '搜索全部物品'
+        [// 1 1
+          '二','四'
         ]
     ],
   ];
 
   Page({
     data: {
+      items: [
+        {value: '0', name: '清水河校区'},
+        {value: '1', name: '沙河校区', checked: 'true'}
+      ],
       multiArray1: [
         ['电子设备', '证件','个人物品','学习用品','全部物品'],
         ['手机', '平板电脑', 'kindle', '笔记本电脑', '耳机','充电器','充电宝','数据线','手表','u盘','鼠标','键盘','触控笔','其他']
     ],
       multiArray2: [
-        ['电子设备', '证件','个人物品','学习用品','全部物品'],
-        ['手机', '平板电脑', 'kindle', '笔记本电脑', '耳机','充电器','充电宝','数据线','手表','u盘','鼠标','键盘','触控笔','其他']
+        ['宿舍区', '教学区'],
+        ['14栋', '16栋']
     ],
+      multiArray3: [
+        ['可选','宿舍区', '教学区'],
+        []
+    ],
+      multiArray4: [
+        ['可选','宿舍区', '教学区'],
+        []
+    ],
+    array: ['上午', '下午', '晚上'],
+    index: 0,
     multiIndex1: [0, 0, 0],
     multiIndex2: [0, 0, 0],
+    multiIndex3: [0, 0, 0],
+    multiIndex4: [0, 0, 0],
+    date: '2021-05-01',
+    dateShow: '05-01',
     time: '12:01',
     timeStart: '00:00',
     timeEnd: '23:59'
@@ -63,7 +100,30 @@ var ms2 = [
     onLoad: function () {
       console.log('Welcome to Mini Code')
     },
-
+    bindDateChange: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      let myDate=String(e.detail.value).substr(5)
+      this.setData({
+          date: e.detail.value,
+          dateShow: myDate
+      })
+    },
+    bindPickerChange: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+          index: e.detail.value
+      })
+    },
+    radioChange: function(e) {
+      console.log('Radio 发生 change 事件，value 值为：', e.detail.value)
+      var items = this.data.items;
+      for (var i = 0, len = items.length; i < len; ++i) {
+        items[i].checked = items[i].value === e.detail.value
+      }
+      this.setData({
+        items: items
+      });
+    },
     bindTimeChange: function (e) {
       console.log('time picker发送选择改变，携带值为', e.detail.value)
       this.setData({
@@ -80,6 +140,18 @@ var ms2 = [
         console.log('picker2发送选择改变，携带值为', e.detail.value)
         this.setData({
             multiIndex2: e.detail.value
+        })
+    },
+    bindMultiPickerChange3: function (e) {
+        console.log('picker3发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            multiIndex3: e.detail.value
+        })
+    },
+    bindMultiPickerChange4: function (e) {
+        console.log('picker4发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            multiIndex4: e.detail.value
         })
     },
     bindMultiPickerColumnChange1: function (e) {
@@ -126,6 +198,58 @@ var ms2 = [
             case 1:
                 data.multiIndex2[1] = e.detail.value;
                 data.multiIndex2[2] = 0;
+                // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
+                break;
+            case 2:
+                // data.multiIndex[2] = e.detail.value;
+                break;
+        }
+        this.setData(data);
+      },
+    bindMultiPickerColumnChange3: function (e) {
+        // return;
+        console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+        var data = {
+            multiArray3: this.data.multiArray3,
+            multiIndex3: this.data.multiIndex3
+        };
+        switch (e.detail.column) {
+            case 0:
+                data.multiIndex3[0] = e.detail.value;
+                data.multiIndex3[1] = 0;
+                data.multiIndex3[2] = 0;
+                data.multiArray3[1] = ms3[1][data.multiIndex3[0]];
+                // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
+                break;
+            case 1:
+                data.multiIndex3[1] = e.detail.value;
+                data.multiIndex3[2] = 0;
+                // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
+                break;
+            case 2:
+                // data.multiIndex[2] = e.detail.value;
+                break;
+        }
+        this.setData(data);
+      },
+    bindMultiPickerColumnChange4: function (e) {
+        // return;
+        console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+        var data = {
+            multiArray4: this.data.multiArray4,
+            multiIndex4: this.data.multiIndex4
+        };
+        switch (e.detail.column) {
+            case 0:
+                data.multiIndex4[0] = e.detail.value;
+                data.multiIndex4[1] = 0;
+                data.multiIndex4[2] = 0;
+                data.multiArray4[1] = ms4[1][data.multiIndex4[0]];
+                // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
+                break;
+            case 1:
+                data.multiIndex4[1] = e.detail.value;
+                data.multiIndex4[2] = 0;
                 // data.multiArray[2] = ms[2][data.multiIndex[0]][data.multiIndex[1]];
                 break;
             case 2:
