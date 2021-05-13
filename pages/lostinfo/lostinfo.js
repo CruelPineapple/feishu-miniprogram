740491953d2f2a3cb9d5d58a86ac8deccf755b9d
OMEN2 <1617218599@qq.com>
2021-05-13 23:44:44 +0800

    lost info finish frame

--- pages/lostinfo/lostinfo.js
+++ pages/lostinfo/lostinfo.js
@@ -2,6 +2,9 @@ Page({
     data: {
         lostInfo: undefined
     },
+    handleRequestPosition: function(){
+
+    },
     onLoad: function(options){
         let app = getApp();
         this.setData({
