e3424f778ff386334e376494ee4f8e585418b3fd
OMEN2 <1617218599@qq.com>
2021-05-19 13:16:24 +0800

    found by self

--- pages/notmatch/notmatch.js
+++ pages/notmatch/notmatch.js
@@ -6,7 +6,38 @@ Page({
         matched:undefined
     },
 
-
+    handleFound:function(){
+        let myThis=this;
+        tt.showModal({
+            title:'提示',
+            content:'您自己找到了失物并希望撤销这条失物查询？',
+            
+          success: (res) => {
+            if(res.confirm){
+                tt.request({
+                  url: 'https://www.fengzigeng.com/api/miniapp/foundBySelf', // 目标服务器url
+                  method: 'POST',
+                  header:{
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  },
+                  data:{
+                    'LostId':myThis.data.lostInfo.ID
+                  },
+                  success: (res) => {
+                    if(res.data.code==200){
+                        tt.showToast({
+                          title: '认领成功', // 内容
+                          success: (res) => {
+                            
+                          }
+                        });
+                    }
+                  }
+                });
+            }
+          }
+        });
+    },
 
     onLoad: function(options){
         let app = getApp();
