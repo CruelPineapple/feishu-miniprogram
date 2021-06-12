e9e932a338076a0766025ae0155720d1e3f98d46
OMEN2 <1617218599@qq.com>
2021-06-13 00:44:29 +0800

    check auth

--- pages/more/more.js
+++ pages/more/more.js
@@ -12,28 +12,29 @@ Page({
         })
     },
     admin:function(){
-      tt.navigateTo({
-        url: '/pages/admin/admin' // 指定页面的url
-      });
-        // tt.request({
-        //   url: 'https://www.fengzigeng.com/api/miniapp/GetPermisson', // 目标服务器url
-        //   method: 'GET',
-        //   success: (res) => {
-        //     if(res.data.code==200){
-        //         tt.navigateTo({
-        //           url: '/pages/admin/admin' // 指定页面的url
-        //         });
-        //     }else{
-        //         tt.showModal({
-        //           title:"提示",
-        //           content:"您没有此权限",
-        //           showCancel:false,
-        //           success: (res) => {
+      // tt.navigateTo({
+      //   url: '/pages/admin/admin' // 指定页面的url
+      // });
+
+        tt.request({
+          url: 'https://www.fengzigeng.com/api/miniapp/GetPermisson', // 目标服务器url
+          method: 'GET',
+          success: (res) => {
+            if(res.data.code==200){
+                tt.navigateTo({
+                  url: '/pages/admin/admin' // 指定页面的url
+                });
+            }else{
+                tt.showModal({
+                  title:"提示",
+                  content:"您没有此权限",
+                  showCancel:false,
+                  success: (res) => {
                     
-        //           }
-        //         });
-        //     }
-        //   }
-        // });
+                  }
+                });
+            }
+          }
+        });
     }
 })
