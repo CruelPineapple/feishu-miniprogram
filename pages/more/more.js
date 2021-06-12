0b6b8fd5a852d7cfa13673ea6e6db4e8c72c6823
OMEN2 <1617218599@qq.com>
2021-06-12 22:50:01 +0800

    more page to admin

---
+++ pages/more/more.js
@@ -0,0 +1,36 @@
+Page({
+    feedback: function(){
+        tt.openSchema({
+            schema: 'https://wenjuan.feishu.cn/m/cfm?t=s4Ihv8ra93ri-euhz',
+            external: false,
+            success (res) {
+                console.log(`res`);
+            },
+            fail (res) {
+                console.log(`reLaunch failure`);
+            }
+        })
+    },
+    admin:function(){
+        tt.request({
+          url: 'https://www.fengzigeng.com/api/miniapp/GetPermisson', // 目标服务器url
+          method: 'GET',
+          success: (res) => {
+            if(res.data.code==200){
+                tt.navigateTo({
+                  url: '/pages/admin/admin' // 指定页面的url
+                });
+            }else{
+                tt.showModal({
+                  title:"提示",
+                  content:"您没有此权限",
+                  showCancel:false,
+                  success: (res) => {
+                    
+                  }
+                });
+            }
+          }
+        });
+    }
+})
